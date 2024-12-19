499a07ca118e5e862aed74bc1b93f3e973780323
AniyaFun <drgithub@outlook.com>
2024-12-19 23:52:14 +0800

    fix: canonical not right

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -5,7 +5,6 @@
 import * as React from 'react';
 import Head from 'next/head';
 import {withRouter, Router} from 'next/router';
-import {finishedTranslations} from 'utils/finishedTranslations';
 
 export interface SeoProps {
   title: string;
